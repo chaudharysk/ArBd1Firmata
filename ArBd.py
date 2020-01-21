d1f5ca4dd49e17f36d6d04d889c628a52deaf1a5
Shashikant Chaudhary <47478378+GhostRiderSK@users.noreply.github.com>
2020-01-21 17:48:08 +0530

    Update ArBd.py

--- ArBd.py
+++ ArBd.py
@@ -82,8 +82,38 @@ class ArBd1:
             value = self.board.analog[7].read()
             print(value)
 
-    def charlieplexing(self):
-        pass
+    def charlieplexing(self,A2='Z',A3='Z',A4='L',A5='H'):
+        # Z is for high impedence or Input
+        # H = HIGH PIN
+        # L = LOW PIN
+        LED=0
+        cmd=0x01 # Command for Charlieplexing
+        if (A2=='Z' and A3=='Z' and A4=='L' and A5=='H'):
+            LED=1
+        elif (A2=='Z' and A3=='L' and A4=='Z' and A5=='H'):
+            LED =2
+        elif (A2=='L' and A3=='Z' and A4=='Z' and A5=='H'):
+            LED=3
+        elif (A2=='Z' and A3=='L' and A4=='H' and A5=='Z'):
+            LED=4
+        elif (A2=='L' and A3=='Z' and A4=='H' and A5=='Z'):
+            LED=5
+        elif (A2=='Z' and A3=='Z' and A4=='H' and A5=='L'):
+            LED=6
+        elif (A2=='L' and A3=='H' and A4=='Z' and A5=='Z'):
+            LED=7
+        elif (A2=='Z' and A3=='H' and A4=='Z' and A5=='0'):
+            LED=8
+        elif (A2=='Z' and A3=='H' and A4=='L' and A5=='Z'):
+            LED=9
+        elif (A2=='H' and A3=='Z' and A4=='Z' and A5=='L'):
+            LED=10
+        elif (A2=='1' and A3=='Z' and A4=='0' and A5=='Z'):
+            LED = 11
+        elif (A2=='H' and A3=='L' and A4=='Z' and A5=='Z'):
+            LED = 12
+        data=[LED]
+        self.board.send_sysex(cmd,data)
 
 
 
